bbc7d2a4ffb553caa6a76417238ab2d3fe991e8b
wenge <2476545423@qq.com>
2024-01-13 01:19:46 +0800

    界面优化

--- card_conf.js
+++ card_conf.js
@@ -1,85 +1,138 @@
 
-const cards = [
+const clazzCard = [
     {
-        "title": "Chat GPT",
-        "show": false,
-        "type": "page",
-        "info": "http://gpt.wenp.top/",
-        "img": ""
-    },
-    {
-        "title": "gpt重构",
+        "className": "工具",
         "show": true,
-        "type": "page",
-        "info": "./pages/gpt/index.html"
+        "children": [
+            {
+                "title": "和风天气",
+                "show": true,
+                "type": "code",
+                "info": "he-plugin-standard",
+                "desc": "简易的天气插件，可以以卡片嵌入到网页中。"
+            },
+            {
+                "title": "电子绘图",
+                "show": true,
+                "type": "page",
+                "info": "https://simulator.io/board",
+                "img": "components/img.png",
+                "desc": "电路仿真在线绘图网站，可以很方便地画自己的逻辑电路并调试。"
+            },
+            {
+                "title": "足球胜平负",
+                "show": true,
+                "type": "page",
+                "info": "./pages/picks_soccer/index.html",
+                "img": "components/img_1.png",
+                "desc": "用于足球胜负平的随机投注，可以显示最新的比赛信息，解决投注选择困难。"
+            },
+            {
+                "title": "Chat GPT",
+                "show": false,
+                "type": "page",
+                "info": "http://gpt.wenp.top/",
+                "img": "",
+                "desc": ""
+            },
+        ]
     },
     {
-        "title": "和风天气",
+        "className": "特效",
         "show": true,
-        "type": "code",
-        "info": "he-plugin-standard"
+        "children": [
+            {
+                "title": "重力笔",
+                "show": true,
+                "type": "page",
+                "info": "./pages/gravity_points/index.html",
+                "img": "pages/gravity_points/img.png",
+                "desc": "这是一个模拟重力的页面，点击屏幕将添加重力，粒子将会在受到重力影响下改变移动轨迹。"
+            },
+            {
+                "title": "滑稽",
+                "show": true,
+                "type": "page",
+                "info": "./pages/comical/index.html",
+                "img": "./pages/comical/images/ParticleSmoke.png",
+                "desc": "滑稽滑稽滑稽滑稽滑稽滑稽滑稽滑稽滑稽滑稽滑稽..."
+            },
+            {
+                "title": "烟花",
+                "show": true,
+                "type": "page",
+                "info": "./pages/firework-simulator-v2/index.html",
+                "img": "./pages/firework-simulator-v2/icon.png",
+                "desc": "一个播放烟花的界面，点击屏幕可以触发烟花效果，也可以打开声音。"
+            },
+            {
+                "title": "粒子效果",
+                "show": true,
+                "type": "page",
+                "info": "./pages/particle_effects/index.html",
+                "img": "pages/particle_effects/img.png",
+                "desc": "各种粒子形状，点击屏幕将会触发一些变化。"
+            },
+            {
+                "title": "万花筒",
+                "show": true,
+                "type": "page",
+                "info": "./pages/kaleidoscope/index.html",
+                "img": "pages/kaleidoscope/img.png",
+                "desc": "万花筒特效，移动鼠标将会产生不同的视觉效果。"
+            },
+            {
+                "title": "八卦时钟",
+                "show": true,
+                "type": "page",
+                "info": "./pages/time8/index.html",
+                "img": "pages/time8/img.png",
+                "desc": "这是一个时钟，以中文八卦的形式来显示时间。"
+            }
+        ]
     },
     {
-        "title": "足球胜平负",
+        "className": "其他",
         "show": true,
-        "type": "page",
-        "info": "./pages/picks_soccer/index.html",
-        "img": ""
-    },
-    {
-        "title": "个人博客",
-        "show": false,
-        "type": "page",
-        "info": "http://bk.wenp.top/",
-        "img": ""
-    },
-    {
-        "title": "重力点测试",
-        "show": true,
-        "type": "page",
-        "info": "./pages/gravity_points/index.html",
-        "img": ""
-    },
-    {
-        "title": "滑稽",
-        "show": true,
-        "type": "page",
-        "info": "./pages/comical/index.html",
-        "img": "./pages/comical/images/ParticleSmoke.png"
-    },
-    {
-        "title": "烟花",
-        "show": true,
-        "type": "page",
-        "info": "./pages/firework-simulator-v2/index.html",
-        "img": ""
-    },
-    {
-        "title": "粒子效果",
-        "show": true,
-        "type": "page",
-        "info": "./pages/particle_effects/index.html",
-        "img": ""
-    },
-    {
-        "title": "万花筒",
-        "show": true,
-        "type": "page",
-        "info": "./pages/kaleidoscope/index.html",
-        "img": ""
-    },
-    {
-        "title": "电子绘图",
-        "show": true,
-        "type": "page",
-        "info": "https://simulator.io/board",
-        "img": ""
-    },
-    {
-        "title": "八卦时钟",
-        "show": true,
-        "type": "page",
-        "info": "./pages/time8/index.html",
-        "img": ""
+        "children": [
+            {
+                "title": "gpt重构",
+                "show": true,
+                "type": "page",
+                "info": "./pages/gpt/index.html",
+                "img": "pages/gpt/img.png",
+                "desc": "对chat-gpt的一个开源项目页面进行重写，仅做展示，实际不提供功能。"
+            },
+            {
+                "title": "个人博客",
+                "show": false,
+                "type": "page",
+                "info": "http://bk.wenp.top/",
+                "img": "",
+                "desc": ""
+            },
+
+        ]
     }
-];
+]
+
+
+// 获取全部card
+function allCard(){
+    const list = []
+    for (let clazzCardElement of clazzCard) {
+        list.push(...clazzCardElement["children"])
+    }
+    return list
+}
+
+// 分类：含全部
+function allAndClassCard() {
+    const list = [{
+        "className": "其他",
+        "show": true,
+        "children": allCard()
+    }]
+    list.push(...clazzCard)
+    return list
+}
---
+++ components/WCard.js
@@ -0,0 +1,67 @@
+/**
+ * card组件
+ */
+class WCard {
+    body = {
+        template: `
+            <el-card v-if="card.show" class="card">
+                <div v-if="card.type === 'code'">
+                    <div :id="card.info"></div>
+                </div>
+                <div v-else-if="card.type === 'page'">
+                    <el-image :src="card.img || './source/imgs/card_img.png'" @click="toPage(card.info, card.noTarget)"></el-image>
+                    <div style="height: 20px;font-size: 12px; text-align: center">{{ card.title }}</div>
+                </div>
+                <el-tooltip class="item" effect="dark" :content="card.desc" placement="bottom">
+                    <div style="
+                        width: 100%;
+                        overflow: hidden;
+                        text-overflow: ellipsis;
+                        white-space: nowrap;
+                        color: #9c9b9b;
+                        font-size: 11px;
+                    ">{{card.desc}}</div>
+                </el-tooltip>
+                
+                <!-- css -->
+                <div v-html="css" />
+            </el-card>
+            
+        `,
+        props: {
+            card:{}
+        },
+        data() {
+            return{
+                css: `
+                    <style>
+                         .card {
+                            line-height: 2;
+                            margin: 5px;
+                            width: 255.75px;
+                            height: 303px;
+                        }
+                        .card:hover {
+                            box-shadow: 0 2px 4px rgba(14, 2, 2, 0.16), 0 0 6px rgba(0, 0, 0, 0.22)
+                        }
+                        .el-image {
+                            width: 215px;
+                            height: 228px;
+                            border-radius: 5px;
+                        }
+                    </style>
+                `
+            }
+        },
+        methods:{
+            toPage(url, noTarget){
+                if (noTarget){
+                    window.location.href = url
+                } else {
+                    window.open(url)
+                }
+            },
+        }
+    }
+
+}
